18dfeeb00a9a9a40e14ee74730f10a90753080aa
Carissa Christian <atmosphereasmr@gmail.com>
2017-12-29 13:35:17 -0700

    try it again

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use( express.static( `${__dirname}/../build` ) );
+app.use( express.static( `${__dirname}/build` ) );
 
 const path = require('path')
 app.get('*', (req, res)=>{
